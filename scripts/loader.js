ac105c57b2a7d3628b62d3009104c6dec80edb54
aditya-dawadikar <aditya.dawadikar@quantiphi.com>
2022-08-20 08:39:21 +0530

    svg animation added

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,30 +1,7 @@
 $(document).ready(() => {
-    // projects.forEach(project => {
-    //     $("#project-grid").append(`
-    //         <div class="col">
-    //             <div class="card">
-    //                 <div class="flip-card">
-    //                     <div class="flip-card-inner">
-    //                     <div class="justify-content-center flip-card-front">
-    //                         <h3>${project.title}</h3>
-    //                         <p>${project.category}</p>
-    //                         <h6 class="text-white">(${project.tech})</h6>
-    //                     </div>
-    //                     <div class="flip-card-back">
-    //                         <div class="description">
-    //                             ${project.description}
-    //                         </div>
-    //                         <a href="${project.link}" target="_blank">LINK</a>
-    //                     </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     `);
-    // })
     projects.forEach(project => {
         $("#project-grid").append(`
-            <div class="col">
+            <div class="col-md-3 justify-content-center">
                     <div class="flip-card">
                         <div class="flip-card-inner">
                         <div class="justify-content-center flip-card-front">
@@ -47,11 +24,9 @@ $(document).ready(() => {
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "300px";
-    // document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    // document.getElementById("main").style.marginLeft = "0";
-  }
+}
